72c3d6d0174885d6f9ecd3b1fc12dda7a060a70d
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-04-08 22:25:50 -0400

    added some test for local storage

--- local-storage/local-storage-utils.js
+++ local-storage/local-storage-utils.js
@@ -1,11 +1,29 @@
-export function saveUser(user) {
-    const json = JSON.stringify(user);
-    localStorage.setItem('user', json);
-}
+const USER = 'USER';
 
 export function getUser() {
-    const json = localStorage.getItem('user');
-    if (!json) return null;
-    const user = JSON.parse(json);
-    return user;
+    const user = localStorage.getItem(USER);
+
+    if (!user) return null;
+
+    const userParsed = JSON.parse(user);
+
+    return userParsed;
+}
+
+export function setUser(user) {
+    const stringifyUser = JSON.stringify(user);
+
+    localStorage.setItem(USER, stringifyUser);
+}
+
+export function updateUsersChoice(questId, choice) {
+    const user = getUser();
+
+    user.hp += choice.hp;
+
+    user.gold += choice.gold;
+
+    user.completed[questId] = true;
+
+    setUser(user);
 }
